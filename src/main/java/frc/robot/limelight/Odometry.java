0101216c6b6d7212ffc11e761b4a94da831a179b
rose <rosemarie_dalton@hotmail.com>
2025-01-13 19:57:51 -0500

    check precedent

--- src/main/java/frc/robot/limelight/Odometry.java
+++ src/main/java/frc/robot/limelight/Odometry.java
@@ -11,7 +11,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.units.DistanceUnit;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
